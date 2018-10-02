36d2c75aba2b203cdf847cd6a3ba03891f6ada12
Erik Perkins <erik.andrew.perkins@gmail.com>
2018-10-02 12:36:50 -0700

    Replace Rails application

--- roles/postgres/files/docker-entrypoint-initdb.d/fixtures.sql
+++ roles/postgres/files/docker-entrypoint-initdb.d/fixtures.sql
@@ -1,7 +1,6 @@
-create user rails_app with password 'rails_app' superuser login;
-create user snap_app with password 'snap_app' createdb login;
 create user phoenix_app with password 'phoenix_app' createdb login;
 
+create user snap_app with password 'snap_app' createdb login;
 create database snap_app;
 alter database snap_app owner to snap_app;
 
@@ -20,19 +19,3 @@ insert into todos (id, text) values
 ;
 
 alter table todos owner to snap_app;
-
-create database rails_app;
-\connect rails_app
-
-grant usage, select
-  on all sequences
-  in schema public
-  to rails_app
-;
-
-alter default privileges
-  in schema public
-  grant usage, select
-    on sequences
-    to rails_app
-;
